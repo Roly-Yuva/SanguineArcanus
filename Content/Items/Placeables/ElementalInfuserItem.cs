9c1e07c0ee970b3e104fb6a7ea783e8f242adae6
Roly-Yuva <codedyuva333@gmail.com>
2022-10-02 18:48:45 -0400

    Created new files

---
+++ Content/Items/Placeables/ElementalInfuserItem.cs
@@ -0,0 +1,35 @@
+using SanguineArcanus.Content.Items.Materials;
+using SanguineArcanus.Content.Tiles.CraftingStations;
+using Terraria;
+using Terraira.ID;
+using Terraria.ModLoader;
+
+namespace SanguineArcanus.Content.Item.Placeables
+{
+    public class ElementalInfuserItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Elemental Infuser");
+            Tooltip.SetDefault("A special furnace that can be used to infuse essence into special stones.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 15;
+            Item.height = 15;
+            Item.value = 10000; // 1 gold
+            Item.rare = ItemRarityID.Blue;
+            Item.createTile = ModContent.TileType<ElementalInfuserTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Furnace);
+            recipe.AddIngredient(ModContent.ItemType<ArcaneSigil>(), 15);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
